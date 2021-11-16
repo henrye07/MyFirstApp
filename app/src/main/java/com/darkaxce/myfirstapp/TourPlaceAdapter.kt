92ce264abd0e71304747c6a51a76bfa28c779270
henrye07 <henryeraso7g@gmail.com>
2021-11-16 11:37:13 -0500

    Adding Justification in Description View

---
+++ app/src/main/java/com/darkaxce/myfirstapp/TourPlaceAdapter.kt
@@ -0,0 +1,41 @@
+package com.darkaxce.myfirstapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+class TourPlaceAdapter(private val tourPlace : List<TourPlace>) : RecyclerView.Adapter<TourPlaceAdapter.PlaceHolder>() {
+
+    override fun onCreateViewHolder( parent: ViewGroup, viewType: Int): PlaceHolder {
+        val view= LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.list_tour_place_design,parent,false)
+        return PlaceHolder(view)
+    }
+
+    override fun getItemCount(): Int = tourPlace.size
+
+    override fun onBindViewHolder(holder: PlaceHolder, position: Int) {
+        holder.render(tourPlace[position])
+    }
+
+    inner class PlaceHolder( view : View) : RecyclerView.ViewHolder(view){
+        private val name : TextView = view.findViewById(R.id.namePOI)
+        private val description: TextView = view.findViewById(R.id.descriptionPOI)
+        private val rating: RatingBar = view.findViewById(R.id.ratingPOI)
+        private val image: ImageView = view.findViewById(R.id.imagePOI)
+
+        fun render(tourPlace : TourPlace){
+            name.text=tourPlace.name
+            description.text=tourPlace.description
+            rating.rating= tourPlace.rating
+            Picasso.get().load(tourPlace.img).into(image)
+        }
+    }
+
+}
